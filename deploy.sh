71b2a2fe1d98a82f34def9dda50b240a5ed94314
DuckZZZzzz <792479256@qq.com>
2025-02-27 18:19:28 +0800

    fix: 修改仓库名称

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,6 @@ cd dist
 git init
 git add .
 git commit -m "deploy"
-git remote add origin git@github.com:DuckZZZzzz/gulu-duck.git
+git remote add origin git@github.com:DuckZZZzzz/gulu-duck-ui.git
 git push origin master -f -u
 cd ..
