fee3e80b6ca27076440aa905ccd90f71713de2a0
DuckZZZzzz <792479256@qq.com>
2025-02-27 20:08:16 +0800

    fix: 修改deploy使它输出预览地址，使代码代码失败时不跳过而是停止执行

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,9 @@
-yarn build
-cd dist
-git init
-git add .
-git commit -m "deploy"
-git remote add origin git@github.com:DuckZZZzzz/gulu-duck-ui.git
-git push origin master -f -u
-cd ..
+yarn build &&
+cd dist &&
+git init &&
+git add . &&
+git commit -m "deploy" && 
+git remote add origin git@github.com:DuckZZZzzz/gulu-duck-ui.git &&
+git push origin master -f -u &&
+cd .. 
+echo https://duckzzzzzz.github.io/gulu-duck-ui/#/
