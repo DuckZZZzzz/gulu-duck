7dcb76352a4dbdbc8bb45d5534f17cf5ca1e7a5b
DuckZZZzzz <792479256@qq.com>
2025-02-28 14:32:02 +0800

    feat: 创建一个rollup.config.js

--- readme.md
+++ readme.md
@@ -1,6 +1,7 @@
 # gulu-duck-ui
-onePiece UI 是一款基于 Vue 3 的 UI 组件库。这款组件库其实是我为了总结自己这段时间学习的 Vue 3 相关的学习感受而写的，全程亲手编写，没有采用任何第三方组件库，包括你现在看到的这个官网也都是我自己写的。所以强烈不建议你将此 UI 库用于生产环境。但如果你是抱着看源代码的目的来的，那么这个库还是值得一看的。源代码放在了 GitHub 上，历史提交信息非常规范，你可以按提交的顺序逐个查看；你也可以直接查看每个组件的源代码和示例，运行方法见下文。
-
+这是一个基于 Vue3 + TypeScript 的 UI 组件库，涵盖了Button、Switch、Dialog、Tabs...等常见UI组件。
+深入研究 Vue 3，使用了 setup、onMounted、ref、computed 等 Compositioin API，模块化的源代码简洁明了，易于阅读。
+我还推出了官方文档，地址为：https://duckzzzzzz.github.io/gulu-duck-ui/#/，可在线查看示例代码，并支持PC 和手机浏览。
 
 ## 安装
 打开终端运行下列命令:
---
+++ rollup.config.js
@@ -0,0 +1,32 @@
+import esbuild from 'rollup-plugin-esbuild';
+import vue from 'rollup-plugin-vue';
+import scss from 'rollup-plugin-scss';
+import dartSass from 'sass';
+import { terser } from 'rollup-plugin-terser';
+
+export default {
+	input: 'src/lib/index.ts',
+	output: {
+        globals: {
+            vue: 'Vue'
+        },
+		name: 'duckui',
+		file: 'dist/lib/duckui.js',
+		format: 'umd',
+        plugins: [terser()]
+	},
+	plugins: [
+		vue({
+			include: /\.vue$/,
+		}),
+		scss({
+            include: /\.scss$/,
+            sass: dartSass,
+		}),
+		esbuild({
+			include: /\.[jt]s$/,
+            minify: process.env.NODE_ENV === 'production',
+			target: 'es2015',
+		})
+	]
+};
